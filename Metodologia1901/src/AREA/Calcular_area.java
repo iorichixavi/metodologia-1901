ebb2286d269eaedfe5eb15177e95eb920f626349
iorichi xavi <iorichi xavi@10.30.13.234>
2018-09-24 17:28:33 -0500

    segunda clase

---
+++ Metodologia1901/src/AREA/Calcular_area.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AREA;
+
+/**
+ *
+ * @author iorichi xavi
+ */
+public class Calcular_area {
+    float radio;
+    float area;
+    void CalcularArea(){
+        //implementacion
+        area=3.1416f*radio*radio;
+    }
+    
+}
